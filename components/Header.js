02be85a8891ac0a4c57b818f7f100ae23ded4d1a
swynnejr <saucersam@gmail.com>
2021-09-16 23:45:07 -0500

    Calendar looking good, and cancels

--- components/Header.js
+++ components/Header.js
@@ -10,17 +10,28 @@ import {
     UserCircleIcon,
     UsersIcon,
 } from '@heroicons/react/solid'
+import { setDate } from "date-fns";
 
 function Header() {
     const [searchInput, setSearchInput] = useState("");
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
 
     const selectionRange = {
-        startDate:startDate,
+        startDate: startDate,
         endDate: endDate,
-        key: 'Selection'
+        key: 'selection'
     }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    const resetInput = () => {
+        setSearchInput("");
+    };
 
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
@@ -56,10 +67,30 @@ function Header() {
                 </div>
             </div>
             {searchInput && (
-                <div>
+                <div className="flex flex-col col-span-3 mx-auto mt-2">
                     <DateRangePicker
                         ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={["#FD5861"]}
+                        onChange={handleSelect}
                     />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className='text-2xl flex-grow font-semibold'>
+                            Number of Guests
+                        </h2>
+                        <UsersIcon className="h-5" />
+                        <input
+                            className='w-12 pl-2 text-lg outline-none text-red-400'
+                            value={noOfGuests}
+                            onChange={(e) => setNoOfGuests(e.target.value)}
+                            type="number"
+                            min={1}
+                        />
+                    </div>
+                    <div className="flex">
+                        <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
+                        <button className="flex-grow text-red-500">Search</button>
+                    </div>
                 </div>
             )}
             
