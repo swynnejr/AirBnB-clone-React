7577112b0096047c9b07ce1aa23de287f4f38c2b
swynnejr <saucersam@gmail.com>
2021-09-16 23:14:26 -0500

    Calendar addition

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useState } from "react";
 import {
     SearchIcon,
     GlobeAltIcon,
@@ -8,6 +12,16 @@ import {
 } from '@heroicons/react/solid'
 
 function Header() {
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    const selectionRange = {
+        startDate:startDate,
+        endDate: endDate,
+        key: 'Selection'
+    }
+
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* Left  */}
@@ -22,7 +36,13 @@ function Header() {
 
             {/* Middle - search */}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search"/>
+                <input
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+                    type="text"
+                    placeholder="Start your search"
+                />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-1.5 cursor-pointer md:mx-2" />
             </div>
 
@@ -35,6 +55,14 @@ function Header() {
                     <UserCircleIcon className="h-6"/>
                 </div>
             </div>
+            {searchInput && (
+                <div>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                    />
+                </div>
+            )}
+            
         </header>
     )
 }
