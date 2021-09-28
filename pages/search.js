95a0185bbb7b5f3f9e3005a96af08530b3aceb72
swynnejr <saucersam@gmail.com>
2021-09-28 18:33:52 -0500

    Buttons with css class

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,27 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer"
 
 function Search() {
     return (
         <div>
             <Header />
+
+            <main className='flex'>
+                <section>
+                    <p className='text-xs'>300+ stays for 5 number of guests</p>
+
+                    <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in outer space</h1>
+                    <div className='hidden lg:inline-flex'>
+                        <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Type of Lodging</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms and Beds</p>
+                        <p className="button">More filters</p>
+                    </div>
+                </section>
+            </main>
+
+            <Footer />
         </div>
     )
 }
