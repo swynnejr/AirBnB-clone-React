e2052e62ae2bb43dd226203b99a9ecbb56e1f415
swynnejr <saucersam@gmail.com>
2021-09-28 23:48:22 -0500

    Location Cards and Loading Bar

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,9 @@ import Header from "../components/Header";
 import Footer from "../components/Footer"
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
 
-function Search() {
+function Search({ searchResults }) {
     const router = useRouter();
     const { location, startDate, endDate, noOfGuests } = router.query;
     const formattedStartDate = format(new Date(startDate), "MMMM dd");
@@ -26,12 +27,38 @@ function Search() {
                         <p className="button">Rooms and Beds</p>
                         <p className="button">More filters</p>
                     </div>
+
+                    <div className="flex flex-col">
+                        {searchResults.map(({ img, location, title, description, star, price, total }) => (
+                            <InfoCard
+                                key={img}
+                                img={img}
+                                location={location}
+                                title={title}
+                                description={description}
+                                star={star}
+                                price={price}
+                                total={total}
+                            />
+                        ))}
+                    </div>
                 </section>
             </main>
 
             <Footer />
         </div>
-    )
+    );
 }
 
-export default Search
+export default Search;
+
+// getServerSideProps(context) << Maybe later?
+export async function getServerSideProps() {
+    const searchResults = await fetch("https://links.papareact.com/isz").then(res => res.json());
+
+    return {
+        props: {
+            searchResults,
+        }
+    }
+}
