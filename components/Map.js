f1c19ad765dd5a73c11218dccb5ac9509bb17770
swynnejr <saucersam@gmail.com>
2021-10-02 14:16:03 -0500

    Tweaking map center

--- components/Map.js
+++ components/Map.js
@@ -15,7 +15,7 @@ function Map({ searchResults }) {
     const [viewport, setViewport] =  useState({
         width: '100%',
         height: '100%',
-        latitude: (center.latitude),
+        latitude: (center.latitude - .15),
         longitude: (center.longitude),
         zoom: 11
     });
