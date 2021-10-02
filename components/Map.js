28d94c0f3a986b5f8b76eba6bfd72366578d6909
swynnejr <saucersam@gmail.com>
2021-10-02 14:13:46 -0500

    tweaked map centering

--- components/Map.js
+++ components/Map.js
@@ -15,7 +15,7 @@ function Map({ searchResults }) {
     const [viewport, setViewport] =  useState({
         width: '100%',
         height: '100%',
-        latitude: (center.latitude - .15),
+        latitude: (center.latitude),
         longitude: (center.longitude),
         zoom: 11
     });
