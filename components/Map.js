d96c71e0e891db9c4cfe21cf58e40a1e665c971b
swynnejr <saucersam@gmail.com>
2021-10-02 14:09:40 -0500

    Markers and popups added to map

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,13 @@
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { useState } from "react";
 import getCenter from 'geolib/es/getCenter';
 
 function Map({ searchResults }) {
+     const [selectedLocation, setSelectedLocation] = useState({});
     
     const coordinates = searchResults.map((result) => ({
-        longitude: result.long,
         latitude: result.lat,
+        longitude: result.long,
     }));
     
     const center = getCenter(coordinates);
@@ -14,8 +15,8 @@ function Map({ searchResults }) {
     const [viewport, setViewport] =  useState({
         width: '100%',
         height: '100%',
-        latitude: center.latitude,
-        longitude: center.longitude,
+        latitude: (center.latitude - .15),
+        longitude: (center.longitude),
         zoom: 11
     });
 
@@ -25,7 +26,37 @@ function Map({ searchResults }) {
             mapboxApiAccessToken={process.env.mapbox_key}
             {...viewport}
             onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        ></ReactMapGL>
+        >
+            { searchResults.map(result => (
+                <div key={result.long}>
+                    <Marker
+                        latitude={result.lat}
+                        longitude={result.long}
+                        offsetLeft={-20}
+                        offsetTop={-10}
+                    >
+                        <p
+                            role="img"
+                            onClick={() => setSelectedLocation(result)}
+                            className='cursor-pointer text-2xl animate-bounce'
+                            aria-label="push-pin"
+                        >📌</p>
+                    </Marker>
+                    {selectedLocation.long === result.long ? (
+                        <Popup
+                            className="max-w-md"
+                            onClose={() => setSelectedLocation({})}
+                            closeOnClick={true}
+                            latitude={result.lat}
+                            longitude={result.long}>
+                            <h3 className= "font-bold">{result.title}</h3>
+                            {result.description}
+                        </Popup>
+                    ) : (false) }
+                </div>
+            )) }
+
+        </ReactMapGL>
     );
 }
 
